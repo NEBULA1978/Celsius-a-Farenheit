edaea3e37d5fea0a6a4251623869730d62de9df7
NextCodigos <nebulanemesis376@gmail.com>
2022-03-26 00:53:38 +0100

    subiendo el 27 archivo

---
+++ programa.py
@@ -0,0 +1,9 @@
+"""
+programa temperatura en grados celsius y muestre conversion
+en grados Farenheit
+
+"""
+
+celsius=int(input("¿Temperatura en Celsius?"))
+farenheit=(celsius * 9/5) + 32
+print(celsius, "grados Celsius son ", farenheit,"grados Farenheit")
